d899a65b87fa0e645c578c6e5e35f550271b3b3c
Dmitriy Miroshnik <42974336+wallhided@users.noreply.github.com>
2023-01-03 15:36:49 +0300

    Update main.tf
    Fix module after resolving conflicts

--- main.tf
+++ main.tf
@@ -59,7 +59,7 @@ module "iam_assumable_role_admin" {
   create_role                   = true
   role_name                     = "${var.cluster_name}_argocd"
   provider_url                  = replace(data.aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
-  role_policy_arns              = [aws_iam_policy.this.arn]
+  role_policy_arns              = []
   oidc_fully_qualified_subjects = ["system:serviceaccount:${local.namespace}:argocd-repo-server"]
   tags                          = var.tags
 }
